f6cf269b02f9c235f220a4996a42942e5c03efd3
Ingólfur Sigfússon <ingolfur@stefna.is>
2025-12-21 11:44:40 +0000

    Set maxDate to 02.01.2026

--- js/schedule.js
+++ js/schedule.js
@@ -28,7 +28,7 @@ const days = [
 	'Laugardagur'
 ];
 
-const datePickerMaxDate = '2025-08-31';
+const datePickerMaxDate = '2026-01-02';
 
 const urlParams = new URLSearchParams(window.location.search);
 const showDatepicker = urlParams.get('datepicker') !== 'false';
